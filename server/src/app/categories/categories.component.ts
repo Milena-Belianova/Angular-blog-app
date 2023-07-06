8a1b535db1729853ba2251c870f884b367fe1d8b
Milena Belianova <milena.belianova@gmail.com>
2023-07-06 12:10:33 +0400

    feat: add ang-blog-dashboard, connect firebase

---
+++ server/src/app/categories/categories.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css'],
+})
+export class CategoriesComponent {
+  constructor(private afs: AngularFirestore) {}
+
+  onSubmit(formData: { value: any }) {
+    let categoryData = {
+      category: formData.value.category,
+    };
+    let subCategoryData = {
+      category: 'subCategory1',
+    };
+    this.afs
+      .collection('categories')
+      .add(categoryData)
+      .then((docRef) => {
+        console.log(docRef);
+
+        // this.afs
+        //   .doc(`categories/${docRef.id}`)
+        //   .collection('subcategories')
+        //   .add(subCategoryData);
+
+        this.afs
+          .collection('categories')
+          .doc(docRef.id)
+          .collection('subcategories')
+          .add(subCategoryData)
+          .then((docRef1) => {
+            console.log(docRef1);
+
+            // this.afs
+            //   .doc(`categories/${docRef.id}/subsubcategories/${docRef1.id}`)
+            //   .collection('subsubcategories')
+            //   .add(subCategoryData);
+
+            this.afs
+              .collection('categories')
+              .doc(docRef.id)
+              .collection('subcategories')
+              .doc(docRef1.id)
+              .collection('subsubcategories')
+              .add(subCategoryData)
+              .then((docRef2) => {
+                console.log('Second level subcategory saved successfully');
+              });
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
---
+++ server/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAisIa9LAw2WkeFd05VaYhmbhG-gA5ZYdo",
+    authDomain: "ang-blog-55fab.firebaseapp.com",
+    projectId: "ang-blog-55fab",
+    storageBucket: "ang-blog-55fab.appspot.com",
+    messagingSenderId: "1008743248454",
+    appId: "1:1008743248454:web:1e5c7bb5ef6039f0fb1047"
+  }
+}
